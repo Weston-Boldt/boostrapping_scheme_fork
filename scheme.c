a1608899dc805bcbdafacba05a85c88d40dc7e62
weston <boldt.weston@gmail.com>
2018-04-12 00:47:35 -0500

    got pairs going
    so the way that i'm thinking the way these recursive functions work
    ( again i'm trying to learn here! )
    is that:
    
            ok you got your (1 2) you typed in
            so now that goes to the read function
                    and when you get deep in the read function
                    it notices the first char was a (
                    so it calls read_pair
                    which moves along using getc to go forward along
                    and setting the correct data
    
    also #TODO FIXME i'm super sleepy so i'll ammend this commit later....
        http://peter.michaux.ca/articles/scheme-from-scratch-bootstrap-v0_6-pairs

--- scheme.c
+++ scheme.c
@@ -1,5 +1,4 @@
 // this is based off of bootsrap-scheme
-//
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,12 +7,17 @@
 
 // MODEL
 
+// typedefs
+//      note:
+//          wondering if it would be better
+//          to not typedef the object struct?
 typedef enum {
     THE_EMPTY_LIST,
     BOOLEAN,
     FIXNUM,
     CHARACTER,
-    STRING
+    STRING,
+    PAIR
 } object_type;
 
 typedef struct object
@@ -37,8 +41,61 @@ typedef struct object
             char *value;
         } string;
 
+        // now we are getting into the meat & potatoes
+        //      in a sense
+        struct {
+            struct object *car;
+            struct object *cdr;
+        } pair;
+
     } data;
 } object;
+
+// function prototypes
+
+//   types & stuff
+object *alloc_object(void);
+char is_the_empty_list(object *obj);
+char is_boolean(object *obj);
+char is_false(object *obj);
+char is_true(object *obj);
+object *make_character(char value);
+char is_character(object *obj);
+object *make_string(char *value);
+char is_string(object *obj);
+
+// specific lispy stuff
+object *cons(object *car, object *cdr);
+char is_pair(object *obj);
+object *car(object *pair);
+void set_car(object *obj, object *value);
+object *cdr(object *pair);
+void set_cdr(object *obj, object *value);
+object *make_fixnum(long value);
+char is_fixnum(object *obj);
+
+// read
+void init(void);
+char is_delimiter(int c);
+int peek(FILE *in);
+void eat_whitespace(FILE *in);
+void eat_expected_string(FILE *in, char *str);
+void peek_expected_delimiter(FILE *in);
+object *read_character(FILE *in);
+object *read(FILE *in);
+object *read_pair(FILE *in);
+
+// eval
+object *eval(object *exp);
+
+// print
+void write(object *obj);
+void write_pair(object *pair);
+
+// loop
+int main(void);
+
+
 
 object *alloc_object(void)
 {
@@ -115,6 +172,72 @@ char is_string(object *obj)
 {
     return obj->type == STRING;
 }
+
+object *cons(object *car, object *cdr)
+{
+    object *obj;
+
+    obj = alloc_object();
+    obj->type = PAIR;
+    obj->data.pair.car = car;
+    obj->data.pair.cdr = cdr;
+    return obj;
+}
+
+char is_pair(object *obj)
+{
+    return obj->type == PAIR;
+}
+
+object *car(object *pair) {
+    return pair->data.pair.car;
+}
+
+void set_car(object *obj, object *value)
+{
+    obj->data.pair.car = value;
+}
+
+object *cdr(object *pair) {
+    return pair->data.pair.cdr;
+}
+
+void set_cdr(object *obj, object *value)
+{
+    obj->data.pair.cdr = value;
+}
+
+// some lisp caar cadr etc...
+#define caar(obj) car(car(obj))
+#define cadr(obj) car(cdr(obj))
+#define cdar(obj) cdr(car(obj))
+#define cddr(obj) cdr(cdr(obj))
+// todo use caar and cddr etc macros
+#define caaar(obj) car(car(car(obj)))
+#define caadr(obj) car(car(cdr(obj)))
+#define cadar(obj) car(cdr(car(obj)))
+#define caddr(obj) car(cdr(cdr(obj)))
+#define cdaar(obj) cdr(car(car(obj)))
+#define cdadr(obj) cdr(car(cdr(obj)))
+#define cddar(obj) cdr(cdr(car(obj)))
+#define cdddr(obj) cdr(cdr(cdr(obj)))
+// TODO FIXME test
+#define caaaar(obj) caar(caar(obj))
+#define caaadr(obj) caar(cadr(obj))
+#define caadar(obj) caar(cdar(obj))
+#define caaddr(obj) caar(cddr(obj))
+#define cadaar(obj) cadr(caar(obj))
+#define cadadr(obj) cadr(cadr(obj))
+#define caddar(obj) cadr(cdar(obj))
+#define cadddr(obj) cadr(cddr(obj))
+#define cdaaar(obj) cdar(caar(obj))
+#define cdaadr(obj) cdar(cadr(obj))
+#define cdadar(obj) cdar(cdar(obj))
+#define cdaddr(obj) cdar(cddr(obj))
+#define cddaar(obj) cddr(caar(obj))
+#define cddadr(obj) cddr(cadr(obj))
+#define cdddar(obj) cddr(cdar(obj))
+#define cddddr(obj) cddr(cddr(obj))
 
 object *make_fixnum(long value)
 {
@@ -297,6 +420,37 @@ object *read(FILE *in)
                 exit(1);
         }
     }
+    // fixnum
+    else if (isdigit(c) || (c == '-' && (isdigit(peek(in)))))
+    {
+        // read a fixnum
+        if (c == '-')
+        {
+            sign = -1;
+        }
+        else
+        {
+            ungetc(c, in);
+        }
+        while (isdigit(c = getc(in)))
+        {
+            // note by weston
+            //      i think this is to make the c become a big num
+            num = (num * 10) + (c - '0');
+        }
+        num *= sign;
+        if (is_delimiter(c))
+        {
+            ungetc(c, in);
+            return make_fixnum(num);
+        }
+        else
+        {
+            fprintf(stderr, "number not followed by delimiter\n");
+            exit(1);
+        }
+    }
+    // string
     else if (c == '"')
     {
         i = 0;
@@ -330,57 +484,70 @@ object *read(FILE *in)
         buffer[i] = '\0';
         return make_string(buffer);
     }
-    // empty list
+    // empty list or pair
     else if (c == '(')
     {
-        eat_whitespace(in);
-        c = getc(in);
-        if (c == ')')
-        {
-            return the_empty_list;
-        }
-        else
-        {
-            fprintf(stderr, "unexpected character '%c'. Expecting ')'\n",c);
-            exit(1);
-        }
+        return read_pair(in);
     }
-    else if (isdigit(c) || (c == '-' && (isdigit(peek(in)))))
+    else
     {
-        // read a fixnum
-        if (c == '-')
-        {
-            sign = -1;
-        }
-        else
-        {
-            ungetc(c, in);
-        }
-        while (isdigit(c = getc(in)))
-        {
-            // note by weston
-            //      i think this is to make the c become a big num
-            num = (num * 10) + (c - '0');
-        }
-        num *= sign;
-        if (is_delimiter(c))
-        {
-            ungetc(c, in);
-            return make_fixnum(num);
-        }
-        else
-        {
-            fprintf(stderr, "number not followed by delimiter\n");
-            exit(1);
-        }
-    }
-    else {
-        fprintf(stderr, "bad input. unexpected '%c'\n", c);
+        fprintf(stderr, "bad input. Unexpected '%c'.\n",c);
+        exit(1);
     }
     fprintf(stderr, "read illegal state\n");
     exit(1);
 }
 
+object *read_pair(FILE *in)
+{
+    int c;
+
+    object *car_obj;
+    object *cdr_obj;
+
+    eat_whitespace(in);
+
+    // check for empty list
+    c = getc(in);
+    // THE EMPTY LIST dun dun dun!!!!
+    if (c == ')') {
+        return the_empty_list;
+    }
+    ungetc(c, in);
+
+    car_obj = read(in);
+
+    eat_whitespace(in);
+
+    c = getc(in);
+    // read improper list
+    if (c == '.')
+    { 
+        c = peek(in);
+        if (!is_delimiter(c))
+        {
+            fprintf(stderr, "dot not followed by delimiter\n");
+            exit(1);
+        }
+        cdr_obj = read(in);
+        eat_whitespace(in);
+        c = getc(in); 
+        if (c != ')')
+        {
+            fprintf(stderr,
+                "no trailing right paren\n");
+            exit(1);
+        }
+        return cons(car_obj, cdr_obj);
+    }
+    // read the list
+    else
+    {
+        ungetc(c, in);
+        cdr_obj = read_pair(in);
+        return cons(car_obj, cdr_obj);
+    }
+}
 // evaluate
 
 // just echos until list and symbols
@@ -449,9 +616,39 @@ void write(object *obj)
                     break;
             }
             break;
+        case PAIR:
+            printf("(");
+            write_pair(obj);
+            printf(")");
+            break;
         default:
             fprintf(stderr, "cannot write unkown type\n");
             exit(1);
+    }
+}
+
+void write_pair(object *pair)
+{
+    object *car_obj;
+    object *cdr_obj;
+
+    car_obj = car(pair);
+    cdr_obj = cdr(pair);
+
+    write(car_obj);
+    if (cdr_obj->type == PAIR)
+    {
+        printf(" ");
+        write_pair(cdr_obj);
+    }
+    else if (cdr_obj->type == THE_EMPTY_LIST)
+    {
+        return;
+    }
+    else
+    {
+        printf(" . ");
+        write(cdr_obj);
     }
 }
 
